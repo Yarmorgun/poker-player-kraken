4fa3fca490772b7ecf6623502599c5d7cea57485
Maksym Makarychev <Maksym Makarychev>
2016-07-09 15:36:03 +0300

    inc version, remove old code

--- player.py
+++ player.py
@@ -5,36 +5,9 @@ import probability_analysis as pa
 import game_state_parser as gsp
 import json
 
-# // test bot logic
-def bot_logic(game_state):
-    return 0
-	# // todo: need parsing game_state: is_someone_allin(), is_someone_rise(), get_active_players(), get_all_money_in_game()
-	# // todo: get_probabillity()
-	# // todo:
-	#fold = 0
-	#someone_allin = True # is_someone_allin()
-	#noone_rise = False # is_someone_rise()
-	# active_player_count = # get_active_players()
-	# our_probabillity_to_win = get_probabillity()
-	# current_pot = get_all_money_in_game()
-	# if someone_allin:
-	# 	if is_good_chance_to_win():
-	# 		return all_in
-	# 	else
-	# 		return fold
-	# elif noone_rise:
-	# 	return big_blind * 3
-	# else:
-	# 	amount_to_call = get_max_rise();
-	# 	if is_probabillity_good_to_play():
-	# 		return amount_to_call
-	
-	
-	
-
 class Player:
 
-    VERSION = "1.4"
+    VERSION = "1.5"
     NAME = "Kraken"
 
     def __init__(self):
