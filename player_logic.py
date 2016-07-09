712955dca66348867d9518afa3ce2ba372563d51
Maksym Makarychev <Maksym Makarychev>
2016-07-09 13:58:24 +0300

    add: game_state parser, player logic

---
+++ player_logic.py
@@ -0,0 +1,34 @@
+import probability_analysis
+import game_state_parser as gsp
+
+
+# // test bot logic
+def bot_logic(game_state):
+    # todo: need parsing game_state: is_someone_allin(), is_someone_rise(), get_active_players(), get_all_money_in_game()
+    # todo: get_probabillity()
+    # todo:
+
+    fold = 0
+
+# someone_allin = gsp.is_someone_allin(game_state)
+# noone_rise = gsp.is_someone_raise(game_state)
+# active_player_count =  gsp.get_active_players(game_state)
+
+#    my_pair = get_my_card()
+#    exist_cards = get_table_cards()
+# getProbability(countOfPlayers, distrib, myPair, existOnTable):
+#	our_probabillity_to_win = getProbabillity(active_player_count, 1000, my_pair, exist_cards)
+# current_pot = get_all_money_in_game()
+# if someone_allin:
+# 	if is_good_chance_to_win():
+# 		return all_in
+# 	else
+# 		return fold
+# elif noone_rise:
+# 	return big_blind * 3
+# else:
+# 	amount_to_call = get_max_rise();
+# 	if is_probabillity_good_to_play():
+# 		return amount_to_call
+
+
