f04cae7201fa2b73c3dff7c2e26069d6f29a21e3
FastAnt <Artemrukikruki@gmail.com>
2016-07-09 11:12:52 +0300

    defines

--- probability_analysis.py
+++ probability_analysis.py
@@ -1,1 +1,33 @@
+import numpy as np
+#suits idx
+heart             = 0
+diamond           = 1
+club              = 2
+spade             = 3
+
+# ranks idx
+Two     =   0
+Three   =   1
+Four    =   2
+Five    =   3
+Six     =   4
+Seven   =   5
+Eight   =   6
+Nine    =   7
+Ten     =   8
+Jack    =   9
+Queen   =   10
+King    =   11
+Ace     =   12
+
+# combinations idx
+Royal_F         = 9
+Straight_F      = 8
+Four_of_K       = 7
+Full_House      = 6
+Flash           = 5
+Straight        = 4
+Three_of_a_Kind = 3
+Two_pair        = 2
+One_pair        = 1
 
