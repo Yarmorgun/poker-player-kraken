e0a8e3238b70b34a5b0cf40e510a1c752916b003
Yarmorgun <yarmorgun@gmail.com>
2016-07-09 17:39:41 +0300

    Revert "raise probability"
    This reverts commit 67f85b58ee44efe77a7170071b18b1831987fcce.
    
    # Conflicts:
    #	player.py

--- player.py
+++ player.py
@@ -1,13 +1,39 @@
 import converters
 import parser
 import sys
-import probability_analysis as pa
-import game_state_parser as gsp
-import json
+
+
+# // test bot logic
+def bot_logic(game_state):
+    return 0
+
+
+# // todo: need parsing game_state: is_someone_allin(), is_someone_rise(), get_active_players(), get_all_money_in_game()
+# // todo: get_probabillity()
+# // todo:
+# fold = 0
+# someone_allin = True # is_someone_allin()
+# noone_rise = False # is_someone_rise()
+# active_player_count = # get_active_players()
+# our_probabillity_to_win = get_probabillity()
+# current_pot = get_all_money_in_game()
+# if someone_allin:
+# 	if is_good_chance_to_win():
+# 		return all_in
+# 	else
+# 		return fold
+# elif noone_rise:
+# 	return big_blind * 3
+# else:
+# 	amount_to_call = get_max_rise();
+# 	if is_probabillity_good_to_play():
+# 		return amount_to_call
+
+
+
 
 class Player:
-
-    VERSION = "1.6"
+    VERSION = "1.5"
     NAME = "Kraken"
 
     def __init__(self):
@@ -17,34 +43,32 @@ class Player:
         self.active_players = 0
         self.our_player = None
 
-
     def get_preflop_probability(self, hand, players_count):
         preflop_probability_table = parser.Parser().parse_preflop("preflop.txt")
         hand_converted = converters.server_to_table(hand)
-        print "HAND_CONVERTED:", hand_converted
+        print
+        "HAND_CONVERTED:", hand_converted
         try:
-            preflop_probability = preflop_probability_table[hand_converted][players_count-1]
+            preflop_probability = preflop_probability_table[hand_converted][players_count - 1]
         except:
-            print "START_CONVERTING"
+            print
+            "START_CONVERTING"
             hand_converted_2 = hand_converted[1] + hand_converted[0]
             if len(hand_converted) == 3:
                 hand_converted_2 += hand_converted[2]
-            print "HAND_CONVERTED_2:", hand_converted_2
+            print
+            "HAND_CONVERTED_2:", hand_converted_2
             try:
-                preflop_probability = preflop_probability_table[hand_converted_2][players_count-1]
-            except KeyError:
-                hand_converted[0], hand_converted[1] = hand_converted[1], hand_converted[0]
-            try:
-                preflop_probability = preflop_probability_table[hand_converted][players_count-1]
+                preflop_probability = preflop_probability_table[hand_converted_2][players_count - 1]
             except:
                 preflop_probability = 100
 
-        print "PREFLOP_PROB_EXIT"
+        print
+        "PREFLOP_PROB_EXIT"
         return preflop_probability
 
     def checkBet(self):
         print >> sys.stderr, "CHECK_BET"
-
         current_buy_in = self.game_state["current_buy_in"]
         if current_buy_in == self.our_player["bet"]:
             self.bet = 0
@@ -62,15 +86,23 @@ class Player:
     def raiseBet(self):
         print >> sys.stderr, "RAISE_BET"
         self.bet = self.game_state['current_buy_in'] - self.our_player['bet'] + self.game_state['minimum_raise']
-        self.bet = 3*self.bet
 
     def all_in(self):
         print >> sys.stderr, "ALL_IN"
-
         if self.our_player:
             our_stack = self.our_player["stack"]
             self.bet = our_stack
             return self.bet
+
+    def maxStack(self):
+        players_list = self.game_state["players"]
+        maxStack = 0
+        for pl in players_list:
+            if pl["status"] == "active" and pl["name"] != Player.NAME:
+                if pl["stack"] > maxStack:
+                    maxStack = pl["stack"]
+
+        return maxStack
 
     def betRequest(self, game_state):
         self.game_state = game_state
@@ -86,106 +118,34 @@ class Player:
                     self.our_player = player
                     break
 
-            print "ACTIVE PLAYERS: ", self.active_players
+            print
+            "ACTIVE PLAYERS: ", self.active_players
             hand = self.our_player["hole_cards"]
-            preflop_probability = 0.0
-            if len(game_state["community_cards"]) == 0:
-                preflop_probability = self.get_preflop_probability(hand, self.active_players)
-            else:
-                preflop_probability = pa.getProbability(self.active_players, 700,
-                                                        converters.server_to_propobility_gen(hand),
-                                                        converters.server_to_propobility_gen(
-                                                            game_state["community_cards"]))
-            print >> sys.stderr, "TOR_ID:", game_state["tournament_id"], "GAME_ID:", game_state[
-                "game_id"], "HAND:", hand, "TABLE:", game_state["community_cards"], "preflop probability: " + str(
-                preflop_probability)
-            if preflop_probability < 16.0:
+            preflop_probability = self.get_preflop_probability(hand, self.active_players)
+            print >> sys.stderr, "HAND:", hand, "PROBABILITY: " + str(preflop_probability)
+            if preflop_probability < 15.0:
                 self.foldBet()
-            elif preflop_probability >= 16.0 and preflop_probability < 26:
+            elif preflop_probability >= 15.0 and preflop_probability < 20.0:
                 self.checkBet()
-            elif preflop_probability >= 26 and preflop_probability < 38:
+            elif preflop_probability >= 20.0 and preflop_probability < 40.0:
                 self.callBet()
-            elif preflop_probability >= 38.0:
-                self.raiseBet()
-            elif preflop_probability >= 80:
+            elif preflop_probability > 40.0:
+                maxUsersStack = self.maxStack()
+                if maxUsersStack != 0 and maxUsersStack < self.our_player["stack"]:
+                    self.all_in()
+                else:
+                    self.raiseBet()
+            elif preflop_probability == 100:
                 self.all_in()
         except Exception as e:
             print >> sys.stderr, "MAIN EXCEPTION: ", e.message
             self.all_in()
         finally:
-            print "FINAL_BET:", self.bet
-
+            print
+            "FINAL_BET:", self.bet
             return self.bet
 
     def showdown(self, game_state):
-        # print game_state
-        pass
 
-# EXAMPLE TEST bet
-# pl = Player()
-# data = {
-#     "tournament_id":"550d1d68cd7bd10003000003",
-#     "game_id":"550da1cb2d909006e90004b1",
-#     "round":0,
-#     "bet_index":0,
-#     "small_blind": 10,
-#     "current_buy_in": 320,
-#     "pot": 400,
-#     "minimum_raise": 240,
-#     "dealer": 1,
-#     "orbits": 7,
-#     "in_action": 1,
-#     "players": [
-#         {
-#             "id": 0,
-#             "name": "bob",
-#             "status": "active",
-#             "version": "Default random player",
-#             "stack": 1010,
-#             "bet": 320
-#         },
-#         {
-#             "id": 1,
-#             "name": "Kraken",
-#             "status": "active",
-#             "version": "Default random player",
-#             "stack": 1590,
-#             "bet": 80,
-#             "hole_cards": [
-#                 {
-#                     "rank": "6",
-#                     "suit": "hearts"
-#                 },
-#                 {
-#                     "rank": "K",
-#                     "suit": "spades"
-#                 }
-#             ]
-#         },
-#         {
-#             "id": 2,
-#             "name": "Chuck",
-#             "status": "out",
-#             "version": "Default random player",
-#             "stack": 0,
-#             "bet": 0
-#         }
-#     ],
-#     "community_cards": [
-#         {
-#             "rank": "4",
-#             "suit": "spades"
-#         },
-#         {
-#             "rank": "A",
-#             "suit": "hearts"
-#         },
-#         {
-#             "rank": "6",
-#             "suit": "clubs"
-#         }
-#     ]
-# }
-# json_data = json.dumps(data)
-# json_msg = json.loads(json_data)
-# pl.betRequest(json_msg)
+
+pass
