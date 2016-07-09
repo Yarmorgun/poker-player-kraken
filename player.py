59c5a9de2507680b4dde4d1c40da8ee1d0836015
Artem Sliusar <ArtyomSliusar@gmail.com>
2016-07-09 10:59:55 +0300

    first commit

--- player.py
+++ player.py
@@ -1,9 +1,28 @@
 
 class Player:
-    VERSION = "Default Python folding player"
+
+    VERSION = "1.0"
+    NAME = "Kraken"
+
+    def __init__(self):
+        self.bet = 0
+        self.risk = 1.
+        self.accuracy = 1000
+        self.our_player = None
 
     def betRequest(self, game_state):
-        return 0
+        players_list = game_state["players"]
+
+        for player in players_list:
+            if player["name"] == Player.NAME:
+                self.our_player = player
+                break
+
+        if self.our_player:
+            our_stack = self.our_player["stack"]
+            self.bet = our_stack
+        print self.bet
+        return self.bet
 
     def showdown(self, game_state):
         pass
